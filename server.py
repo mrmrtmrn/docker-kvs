4314bdffa82e97f84157c0709328d7b25af4740c
mrmrtmrn <34802008+mrmrtmrn@users.noreply.github.com>
2020-07-19 22:09:19 +0900

    2nd commit
    remove comment

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os, flask
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
 @app.route('/api/v1/hello')
-#@app.route('/')
 def index():
   return 'hello Dockerfile'
 app.run(debug=False, host='0.0.0.0', port=PORT)
